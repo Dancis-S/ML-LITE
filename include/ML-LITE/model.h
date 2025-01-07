ee3315822a13aaa403da9784dc820007433d85fb
Daniel <dancis@DESKTOP-1CC574M.>
2025-01-07 23:56:40 +0000

    Set up initial abc for model

---
+++ include/ML-LITE/model.h
@@ -0,0 +1,22 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <Eigen/Dense>
+#include <string>
+#include <vector>
+
+// ABC for all the models we will implement
+class model {
+public:
+  virtual ~Model() = default;
+
+  virtual void fit(const Eigen::MatrixXd &trix) = 0;
+  virtual Eigen::VectorXd predict(const Eigen::MatrixXd &trix) const = 0;
+
+  virtual double evaluate(const Eigen::MatrixXd &trix) const = 0;
+
+  virtual void save_model(const std::string &file_path) const {}
+  virtual void load_model(const std::string &file_path) {}
+};
+
+#endif
